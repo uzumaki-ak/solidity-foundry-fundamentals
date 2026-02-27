fe543322e26f080386a2f39a365c17370b59abaa
uzumaki-ak <anikydv38@gmail.com>
2026-02-27 08:07:38 +0000

    deployed on testnet from local using quick node

--- script/DeploySimpleStorage.s.sol
+++ script/DeploySimpleStorage.s.sol
@@ -1,5 +1,3 @@
-
-
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.24;
 
@@ -7,11 +5,9 @@ import {Script} from "forge-std/Script.sol";
 import {SimpleStorage} from "../src/SimpleStorage.sol";
 
 contract DeploySimpleStorage is Script {
-function run() external returns (SimpleStorage) {
-  //it sasy that everything after this line inside this func should send to the rpc node, so we can deploy the contract on the blockchain
-  vm.startBroadcast();
-  SimpleStorage simpleStorage = new SimpleStorage();
-  vm.stopBroadcast();
-  return simpleStorage;
-}
+    function run() external {
+        vm.startBroadcast();
+        new SimpleStorage();
+        vm.stopBroadcast();
+    }
 }
